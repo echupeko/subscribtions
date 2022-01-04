15203c711cd9de847ca7c10e34af934339a3fcad
Евгений Чупеко <echupeko@gmail.com>
2022-01-04 15:07:22 +0700

    update project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form name="subForm" @submit.prevent="addSubscription">
+      <input v-model="subscribeName" placeholder="Введите название подписки" type="text">
+      <input v-model.number="amount" type="number">
+      <input v-model="dateStart" type="date">
+      <input v-model="dateEnd" type="date">
+      <input type="submit" value="Добавить">
+    </form>
+    <SubscriptionItem
+      v-for="(item, index) in subscriptions"
+      :key="item.index"
+      :index="index"
+      :item="item"
+      @updateSubscription="updateSubscription"
+      @deleteSubscription="deleteSubscription"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SubscriptionItem from './components/SubscriptionItem.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SubscriptionItem
+  },
+  data() {
+    return {
+      subscriptions: [],
+      subscribeName: '',
+      amount: 0,
+      dateStart: new Date(),
+      dateEnd: new Date()
+    }
+  },
+  mounted() {
+    this.subscriptions = JSON.parse(sessionStorage.getItem('subscriptions'))
+  },
+  methods: {
+    addSubscription() {
+      let subscription = {
+        id: this.subscriptions.length,
+        subscribeName: this.subscribeName,
+        amount: this.amount,
+        dateStart: this.dateStart,
+        dateEnd: this.dateEnd
+      };
+
+      this.subscriptions.push(subscription);
+      this.updateSubscription();
+      this.cleanForm();
+    },
+
+    deleteSubscription(index) {
+      this.subscriptions.splice(index, 1);
+      this.updateSubscriptions();
+    },
+
+    updateSubscription(item, index) {
+      this.subscriptions[index] = item;
+      this.updateSubscriptions();
+    },
+
+    updateSubscriptions() {
+      sessionStorage.setItem('subscriptions', JSON.stringify(this.subscriptions));
+    },
+
+    cleanForm() {
+      this.subscribeName = '';
+      this.amount = null;
+      this.dateStart = null;
+      this.dateEnd = null;
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/SubscriptionItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <form @submit.prevent="updateSubscription">
+      <input v-model="item.subscribeName" @change="isEdited = true" type="text">
+      <input v-model.number="item.amount" @change="isEdited = true" type="number">
+      <input v-model="item.dateStart" @change="isEdited = true" type="date">
+      <input v-model="item.dateEnd" @change="isEdited = true" type="date">
+      <input v-if="isEdited" type="submit" value="Сохранить">
+      <input @click="deleteSubscription" type="button" value="Удалить">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SubscriptionItem',
+  props: ['item', 'index'],
+  data() {
+    return {
+      isEdited: false
+    }
+  },
+  methods: {
+    updateSubscription() {
+      this.$emit('updateSubscription', this.item, this.index)
+      this.isEdited = !this.isEdited;
+    },
+
+    deleteSubscription() {
+      this.$emit('deleteSubscription', this.index)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
